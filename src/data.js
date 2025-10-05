6f24bfc04b5c67925a50824e07fb1fa7b3ab9352
jerryleooo <whilgeek@gmail.com>
2025-10-05 10:02:20 +0800

    upd

--- src/data.js
+++ src/data.js
@@ -142,139 +142,55 @@ export const accounts = {
   }
 
 export const categories = {
-    "data": [
-      {
-        "id": "d360c3a9-caa5-4b7a-b755-9f31b3307c6e",
-        "name": "贷款 - 工行装修款",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "285a6cb4-9be5-4c56-997f-6e753ffe4aa7"
-      },
-      {
-        "id": "09bc4324-f589-4dce-a846-439e1957d8ac",
-        "name": "贷款 - 邮储银行",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "285a6cb4-9be5-4c56-997f-6e753ffe4aa7"
-      },
-      {
-        "id": "46c21516-c489-45d8-8da4-8b448f43ffd7",
-        "name": "贷款 - 杭州联合 50W",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "285a6cb4-9be5-4c56-997f-6e753ffe4aa7"
-      },
-      {
-        "id": "7b874d18-1c71-4d49-9b2b-cacfa931d575",
-        "name": "贷款 - 宁波银行 20W",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "285a6cb4-9be5-4c56-997f-6e753ffe4aa7"
-      },
-      {
-        "id": "0bf26baa-b5b3-436f-b531-a3a06320c314",
-        "name": "贷款 - 汀岸芷兰车位",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "285a6cb4-9be5-4c56-997f-6e753ffe4aa7"
-      },
-      {
-        "id": "1d06a250-d226-463d-99c6-f817eceb4684",
-        "name": "Kid",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
-      },
-      {
-        "id": "b504d685-f6a1-4944-999d-a98d8194402d",
-        "name": "个人保险 - 小望",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "cbadbd8b-3c1f-40e9-85ae-5676ba08fec2"
-      },
-      {
-        "id": "541836f1-e756-4473-a5d0-6c1d3f06c7fa",
-        "name": "Food",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
-      },
-      {
-        "id": "f1116a18-8295-4b41-bb40-063ab4519a11",
-        "name": "贷款 - 汀岸芷兰",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "285a6cb4-9be5-4c56-997f-6e753ffe4aa7"
-      },
-      {
-        "id": "fb311298-37e0-47e3-ada8-fa56939586bf",
-        "name": "重疾险 - Xiaowen",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "cbadbd8b-3c1f-40e9-85ae-5676ba08fec2"
-      },
-      {
-        "id": "55c33f82-f3ab-45c7-a10e-8068444d8618",
-        "name": "Service Fee",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
-      },
-      {
-        "id": "3c1699a5-522a-435e-86dc-93d900a14f0e",
-        "name": "Income",
-        "is_income": true,
-        "hidden": false,
-        "group_id": "2E1F5BDB-209B-43F9-AF2C-3CE28E380C00"
-      },
-      {
-        "id": "506e8d9d-7ed0-4397-84e4-07a9185dc6b2",
-        "name": "Starting Balances",
-        "is_income": true,
-        "hidden": false,
-        "group_id": "2E1F5BDB-209B-43F9-AF2C-3CE28E380C00"
-      },
-      {
-        "id": "6bbd8472-25d4-4cee-8a11-5bd9f7e83d61",
-        "name": "Savings",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "a137772f-cf2f-4089-9432-822d2ddc1466"
-      },
-      {
-        "id": "af375fd4-d759-46b3-bffe-74a856151d57",
-        "name": "Buy Staff",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
-      },
-      {
-        "id": "d4b0f075-3343-4408-91ed-fae94f74e5bf",
-        "name": "Car",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
-      },
-      {
-        "id": "29ec2c58-8cd3-42fe-9187-7b5dfe0f70a6",
-        "name": "Housing",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
-      },
-      {
-        "id": "649411c4-583d-47fd-9c4e-d25eb3697b36",
-        "name": "Social",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
-      },
-      {
-        "id": "860739a1-05b4-4273-a06f-4ffd17f1c5e4",
-        "name": "Hobby",
-        "is_income": false,
-        "hidden": false,
-        "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
-      }
-    ]
-  }
+  "data": [
+    {
+      "id": "541836f1-e756-4473-a5d0-6c1d3f06c7fa",
+      "name": "Food",
+      "is_income": false,
+      "hidden": false,
+      "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
+    },
+    {
+      "id": "af375fd4-d759-46b3-bffe-74a856151d57",
+      "name": "General",
+      "is_income": false,
+      "hidden": false,
+      "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
+    },
+    {
+      "id": "d4b0f075-3343-4408-91ed-fae94f74e5bf",
+      "name": "Bills",
+      "is_income": false,
+      "hidden": false,
+      "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
+    },
+    {
+      "id": "29ec2c58-8cd3-42fe-9187-7b5dfe0f70a6",
+      "name": "Bills (Flexible)",
+      "is_income": false,
+      "hidden": false,
+      "group_id": "fc3825fd-b982-4b72-b768-5b30844cf832"
+    },
+    {
+      "id": "6bbd8472-25d4-4cee-8a11-5bd9f7e83d61",
+      "name": "Savings",
+      "is_income": false,
+      "hidden": false,
+      "group_id": "a137772f-cf2f-4089-9432-822d2ddc1466"
+    },
+    {
+      "id": "3c1699a5-522a-435e-86dc-93d900a14f0e",
+      "name": "Income",
+      "is_income": true,
+      "hidden": false,
+      "group_id": "2E1F5BDB-209B-43F9-AF2C-3CE28E380C00"
+    },
+    {
+      "id": "506e8d9d-7ed0-4397-84e4-07a9185dc6b2",
+      "name": "Starting Balances",
+      "is_income": true,
+      "hidden": false,
+      "group_id": "2E1F5BDB-209B-43F9-AF2C-3CE28E380C00"
+    }
+  ]
+}
